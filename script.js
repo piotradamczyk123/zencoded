df4be558b0626c1ed074b8e3be390abd7f04d25d
Piotr <piotr@MacBook-Pro-Piotr.local>
2022-03-30 22:41:21 +0200

    adding gsap

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function myFunction(x) {
+  if (x.matches) {
+    console.log("");
+  } else {
+    let hamburger = document.getElementsByClassName("header__checkbox-label");
+    hamburger[0].addEventListener("click", () => {
+      console.log("clicked");
+    });
+  }
+}
 
+var x = window.matchMedia("(min-width: 900px)");
+myFunction(x); // Call listener function at run time
