7f0b781a09ac34c43fc899f6332218fd637d96b4
Piotr <piotr@MacBook-Pro-Piotr.local>
2022-03-31 21:36:33 +0200

    header finished

--- script.js
+++ script.js
@@ -6,13 +6,42 @@ function myFunction(x) {
       "header__checkbox-label"
     )[0];
     let background = document.getElementsByClassName("header__background")[0];
+    let links = document.getElementsByClassName("header__link");
+    let linksWrapper = document.getElementsByClassName(
+      "header__links-wrapper"
+    )[0];
+    let delayInMilliseconds = 1000;
+
     hamburger.addEventListener("click", () => {
       if (background.classList.contains("shown")) {
         background.classList.remove("shown");
         background.classList.remove("header__background--visible");
+        background.classList.add("header__background--invisible");
+        for (let index = 0; index < links.length; index++) {
+          const element = links[index];
+          element.classList.remove("header__link--fade-in");
+        }
+
+        for (let index = 0; index < links.length; index++) {
+          const element = links[index];
+          element.classList.add("header__link--fade-out");
+        }
+        setTimeout(function () {
+          linksWrapper.classList.add("header__links-wrapper--hidden");
+        }, delayInMilliseconds);
       } else {
+        linksWrapper.classList.remove("header__links-wrapper--hidden");
         background.classList.add("shown");
+        background.classList.remove("header__background--invisible");
         background.classList.add("header__background--visible");
+        for (let index = 0; index < links.length; index++) {
+          const element = links[index];
+          element.classList.remove("header__link--fade-out");
+        }
+        for (let index = 0; index < links.length; index++) {
+          const element = links[index];
+          element.classList.add("header__link--fade-in");
+        }
       }
     });
   }
