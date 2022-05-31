4cbf9820dc7c5a77c2693546f372c8bbc50e384d
Piotr <piotr@MacBook-Pro-Piotr.local>
2022-05-31 22:50:06 +0200

    modal added

--- script.js
+++ script.js
@@ -760,3 +760,31 @@ card4.addEventListener("click", () => {
     card3.classList.remove("noclick");
   }
 });
+
+//MODAL
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
